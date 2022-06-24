608ccc2cc91f93f06e37a0c8e5d287bd7f3888cf
xianglanqu <quxianglan55@gmail.com>
2022-06-24 11:52:14 +1000

    route path update

--- src/App.js
+++ src/App.js
@@ -32,11 +32,42 @@ const App = () => {
                         </div>
                     )}
                     <div className={`'dark:bg-main-bg bg-main-bg min-h-screen  w-full'
-                        ${activeMenu ? 'md:ml-72' : 'flex-2'}`
-                    }></div>
-                    <div className='fixed md:static
+                        {activeMenu ? 'md:ml-72' : 'flex-2'}`
+                    }>
+                        <div className='fixed md:static
                     bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                        Navbar
+                            Navbar
+                        </div>
+                    </div>
+
+                    <div>
+                        <Routes>
+                            {/* Dashboard-homepage */}
+                            <Route path="/" element="ECommerce" />
+                            <Route path="/ecommerce" element="ECommerce" />
+
+                            {/* Pages */}
+                            <Route path="/orders" element="Orders" />
+                            <Route path="/employees" element="Employees" />
+                            <Route path="/customers" element="Customers" />
+
+                            {/* Apps */}
+                            <Route path="/kanban" element="Kanban" />
+                            <Route path="/editor" element="Editor" />
+                            <Route path="/calendar" element="Calendar" />
+                            <Route path="/color-picker" element="ColorPicker" />
+
+                            {/* Charts */}
+                            <Route path="/line" element="Line" />
+                            <Route path="/area" element="Area" />
+                            <Route path="/bar" element="Bar" />
+                            <Route path="/pie" element="Pie" />
+                            <Route path="/financial" element="Financial" />
+                            <Route path="/color-mapping" element="ColorMapping" />
+                            <Route path="/pyramid" element="Pyramid" />
+                            <Route path="/stacked" element="Stacked" />
+
+                        </Routes>
                     </div>
                 </div>
             </BrowserRouter>

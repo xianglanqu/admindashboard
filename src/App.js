43dd8c8742a67ede51a7c95567547eed1ececf68
xianglanqu <quxianglan55@gmail.com>
2022-06-24 10:57:00 +1000

    create setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { FiSettings } from 'react-icons/fi';
+
 import './App.css'
 const App = () => {
+    const activeMenu = true;
     return (
-        <h1 className='underline text-3xl'>App</h1>
+        <div>
+            <BrowserRouter>
+                <div className='fiex relative dark:bg-main-dark-bg'>
+                    <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+                        <TooltipComponent content="Settings" position="Top">
+                            <button type='button' className='text-3xl p-3
+                                hover:drop-shadow-xl hover:bg-light-gray text-white'
+                                style={{ background: 'blue', borderRadius: '50%' }}>
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
+                    {activeMenu ? (
+                        <div className='w-72 fixed sidebar
+                        dark:bg-secondary-dark-bg
+                        bg-white'>
+                            Sidebar
+                        </div>
+                    ) : (
+                        <div className='w-0 
+                        dark:bg-secondary-dark-bg'>
+                            Sidebar w-0
+                        </div>
+                    )}
+                    <div className={`'dark:bg-main-bg bg-main-bg min-h-screen  w-full'
+                        ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                    }></div>
+                    <div className='fixed md:static
+                    bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                        Navbar
+                    </div>
+                </div>
+            </BrowserRouter>
+        </div>
     )
 }
 

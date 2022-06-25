a0994879b02ab2a8534511adb46d3bba6b6b5e0e
xianglanqu <quxianglan55@gmail.com>
2022-06-25 18:43:47 +1000

    Update ContextProvider.js

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,1 +1,18 @@
 import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+};
+export const ContextProvider = ({ children }) => {
+    return (
+        // eslint-disable-next-line react/jsx-no-constructed-context-values
+        <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
+            {children}
+        </StateContext.Provider>
+    );
+}

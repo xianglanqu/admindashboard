0947c154b6137e68ae5f8ec3d5ace8fbc5241999
xianglanqu <quxianglan55@gmail.com>
2022-06-26 13:23:02 +1000

    NavButton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../contexts/ContextProvider';
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+    <TooltipComponent content={title} position="BottomCenter">
+        <button
+            type="button"
+            onClick={() => customFunc()}
+            style={{ color }}
+            className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+        >
+            <span
+                style={{ background: dotColor }}
+                className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+            />
+            {icon}
+        </button>
+    </TooltipComponent>
+);
 const Navbar = () => {
+    const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
     return (
-        <div>Navbar</div>
+        <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+            Navbar
+        </div>
     )
 }
 
--- src/data/dummy.js
+++ src/data/dummy.js
@@ -12,7 +12,7 @@ import { GiLouvrePyramid } from 'react-icons/gi';
 import { GrLocation } from 'react-icons/gr';
 import avatar from './avatar.jpg';
 import avatar2 from './avatar2.jpg';
-import avatar3 from './avatar3.png';
+import avatar3 from './avatar3.jpg';
 import avatar4 from './avatar4.jpg';
 import product1 from './product1.jpg';
 import product2 from './product2.jpg';

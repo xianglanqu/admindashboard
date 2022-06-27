a9934ffc9743d9686cc08b280eb1100c3a6fda5b
xianglanqu <quxianglan55@gmail.com>
2022-06-28 09:35:42 +1000

    fix bug

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     return (
         <div className={currentMode === 'Dark' ? 'dark' : ''}>
             <BrowserRouter>
-                <div className='fiex relative dark:bg-main-dark-bg'>
+                <div className='flex relative dark:bg-main-dark-bg'>
                     <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
                         <TooltipComponent content="Settings" position="Top">
                             <button type='button' onClick={() => setThemeSettings(true)}
@@ -43,11 +43,14 @@ const App = () => {
                             <Sidebar />
                         </div>
                     )}
-                    <div className={`'dark:bg-main-dark-bg bg-main-bg min-h-screen  w-full'
-                        {activeMenu ? 'md:ml-72' : 'flex-2'}`
-                    }>
-                        <div className='fixed md:static
-                    bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                    <div
+                        className={
+                            activeMenu
+                                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                        }
+                    >
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                             <Navbar />
                         </div>
                         <div>

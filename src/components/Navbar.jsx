c561942d1be89c03f12eeff14695d395c4735d86
xianglanqu <quxianglan55@gmail.com>
2022-06-30 13:17:35 +1000

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import avatar from '../data/avatar.jpg';
+import avatar3 from '../data/avatar3.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -62,13 +62,13 @@ const Navbar = () => {
                     >
                         <img
                             className="rounded-full w-8 h-8"
-                            src={avatar}
+                            src={avatar3}
                             alt="user-profile"
                         />
                         <p>
                             <span className="text-gray-400 text-14">Hi,</span>{' '}
                             <span className="text-gray-400 font-bold ml-1 text-14">
-                                Michael
+                                Nina
                             </span>
                         </p>
                         <MdKeyboardArrowDown className="text-gray-400 text-14" />
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { Button } from '.';
 import { userProfileData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
-import avatar from '../data/avatar.jpg';
+import avatar3 from '../data/avatar3.jpg';
 
 const UserProfile = () => {
     const { currentColor } = useStateContext();
@@ -24,13 +24,13 @@ const UserProfile = () => {
             <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
                 <img
                     className="rounded-full h-24 w-24"
-                    src={avatar}
+                    src={avatar3}
                     alt="user-profile"
                 />
                 <div>
-                    <p className="font-semibold text-xl dark:text-gray-200"> Michael Roberts </p>
+                    <p className="font-semibold text-xl dark:text-gray-200"> Nina </p>
                     <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
-                    <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shop.com </p>
+                    <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shopdashboard.com </p>
                 </div>
             </div>
             <div>
